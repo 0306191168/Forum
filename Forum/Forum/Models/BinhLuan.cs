d82a35e7c3b400cd52492aece71065b25bc8e215
your name <your email>
2021-11-26 09:28:56 +0700

    add file

---
+++ Forum/Forum/Models/BinhLuan.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Forum.Models
+{
+    public class BinhLuan
+    {
+        public int Ma { get; set; }
+
+        public String NoiDung { get; set; }
+
+        public DateTime NgayTao { get; set; }
+
+        public DateTime NgayXoa { get; set; }
+    }
+}
---
+++ Forum/Forum/Models/ThanhVien.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Forum.Models
+{
+    public class ThanhVien
+    {
+        public int Ma { get; set; }
+
+        public int Quyen { get; set; }
+
+        public String TenDangNhap { get; set; }
+
+        public String MatKhau { get; set; }
+
+        public String HoTen { get; set; }
+
+        public String DiaChi { get; set; }
+
+        public String Phone { get; set; }
+    }
+}
